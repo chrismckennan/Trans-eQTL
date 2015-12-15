53208e2b22ccf3d423f17c80c24456ca4f0de00e
chrismckennan <cgm29@uchicago.edu>
2015-12-15 17:29:48 -0600

    Added Gibbs sampler code; modified network expression

--- R/SimulateNetworkExpression.R
+++ R/SimulateNetworkExpression.R
@@ -3,20 +3,20 @@
 
 ###Input: A covariance matrix 'Sigma' for the expression of g and its neighbors, an effect size 'theta' for the cis-eQTL, a partition 'gamma' of genes in the network into Directly affected, Indirectly affected, and Unaffected and a genotype vector for 'n.ind' individuals at s
 
-n.ind <- 1e4		#Number of independent individuals
-n.nei <- 9			#Number of neighbors of g
-f.s <- 0.1			#Minor Allele Frequency
-Sigma <- create.sigma(n.nei+1)
-lambda <- 1		##Shrinkage term; shrink mean of indirect effects
-D.gam <- (4)
-I.gam <- (1:3)			##Indices of indirectly affected neighbors; correspond to indices of Sigma
-U.gam <- (5:(n.nei+1))		##Indices of unaffected neighbors; correspond to indices of Sigma
-theta = 0.1
-X.s <- rbinom(n=n.ind, size=1, prob=f.s) + rbinom(n=n.ind, size=1, prob=f.s)			#Genotype vector of n.ind individuals under HWE
+#n.ind <- 1e5		#Number of independent individuals
+#n.nei <- 9			#Number of neighbors of g
+#f.s <- 0.1			#Minor Allele Frequency
+#Sigma <- create.sigma(n.nei+1)
+#lambda <- 1		##Shrinkage term; shrink mean of indirect effects
+#D.gam <- (4)
+#I.gam <- (1:3)			##Indices of indirectly affected neighbors; correspond to indices of Sigma
+#U.gam <- (5:(n.nei+1))		##Indices of unaffected neighbors; correspond to indices of Sigma
+#theta = 0.1
+#X.s <- rbinom(n=n.ind, size=1, prob=f.s) + rbinom(n=n.ind, size=1, prob=f.s)			#Genotype vector of n.ind individuals under HWE
 
-sigma.a <- c(0.01, 0.05, 0.1)				#Prior sd's on effect; used in BF calculation
-weights.sigma <- c(1, 1, 1)					#Relative weights of each element of sigma.a
-m = n.nei + 1					#degrees of freedom used in Wishart Prior; must be at least n.nei + 1
+#sigma.a <- c(0.4)				#Prior sd's on effect; used in BF calculation
+#weights.sigma <- c(1)					#Relative weights of each element of sigma.a
+#m = n.nei + 1					#degrees of freedom used in Wishart Prior; must be at least n.nei + 1
 
 ###Output is a matrix of log10 Bayes Factors for all 2^n.nei possible configurations
 
@@ -24,17 +24,17 @@ m = n.nei + 1					#degrees of freedom used in Wishart Prior; must be at least n.
 
 ####Simulate expression data for n.ind individuals####
 
-Y.gex <- Sim.gex(Sigma, D.gam, I.gam, U.gam, theta, X.s, lambda=lambda)			#n.ind x (n.nei + 1) gene expression matrix in the order of input Sigma
+#Y.gex <- Sim.gex(Sigma, D.gam, I.gam, U.gam, theta, X.s, lambda=lambda)			#n.ind x (n.nei + 1) gene expression matrix in the order of input Sigma
 
 ####Sufficient statistics necessary to calculate Bayes Factor####
 
-suff.stat <- Suff.stat(Y.gex, X.s)
+#suff.stat <- Suff.stat(Y.gex, X.s)
 
 ####Compute Bayes Factors####
 #Return a 2^n.nei x (n.nei+2) matrix of log10 Bayes factors; the last column are the log10 BF's and the preceding columns give the partitions
 #D.gam indicates the position of the direct effect
 
-BF.obj <- Log10BF(suff.stat, D.gam, n.ind, sigma.a, weights.sigma, m)
+#BF.obj <- Log10BF(suff.stat, D.gam, n.ind, sigma.a, weights.sigma, m)
 
 
 
@@ -73,7 +73,7 @@ Sim.gex <- function(Sigma.in, D.gam, I.gam, U.gam, theta, X.s, lambda=1) {
 	R <- chol(Sigma)			#R'R = Sigma
 	resids.iid <- matrix(rnorm((n.nei + 1)*n.ind), nrow=n.nei+1, ncol=n.ind)
 	resids <- t(R) %*% resids.iid		#Matrix of gene expression residuals, a (n.nei + 1) x n matrix
-	mu.mat <- cbind(mu.vec) %*% rbind(theta*X.s)		#Mean matrix of gene expression values, a (n.nei + 1) x n matrix
+	mu.mat <- cbind(mu.vec) %*% rbind(theta*X.s)		#Mean matrix of gene expression values, a (n.nei + 1) x n.ind matrix
 	
 	Y.gex <- mu.mat + resids			#A (n.nei + 1) x n.ind expression matrix; The columns of Y are (Y_I, Y_D, Y_U)
 	return(t(Y.gex) %*% perm.matrix)		#Rotate Y.gex into the order of Sigma.in; The output is a n.ind x (n.nei + 1) gene expression matrix
@@ -166,5 +166,70 @@ Log10BF <- function(suff.stat, D.gam, n.ind, sigma.a, weights.sigma, m) {
 	BF.mat[,n.nei + 2] <- BF.mat[,n.nei + 2]/log(10)		#Log10 BF
 	
 	return(list(BF.mat = BF.mat, D.ind = D.gam))
+}
+
+
+#Compute log10 Bayes factor for a single partition gamma
+#Inputs are the sufficient statistics, the indices corresponding to the directly affected and unaffected genes, #independent samples, vector of sigma.a's, weights for sigma.a's, m in Wisharg prio
+#Output is the log10BF and the indices corresponding to the partitions
+
+Log10BF.sing <- function(suff.stat, D.gam, U.gam, n.ind, sigma.a, weights.sigma, m) {
+	var.a <- sigma.a^2
+	weights <- weights.sigma/sum(weights.sigma)
+	
+	SYY <- suff.stat$SYY
+	sxx <- suff.stat$sxx
+	SYX <- suff.stat$SYX
+	SY1 <- suff.stat$SY1
+	mu.g <- suff.stat$mu.g
+	
+	n.nei <- dim(SYY)[1] - 1	
+	n.u <- length(U.gam)
+		
+	##Build XX1 = 1/n.ind * [1 Y_u X_s]' [1 Y_u X_s] and XX0 = 1/n.ind * [1 Y_u]' [1 Y_u]##
+		
+	XX1 <- array(1, dim=c(n.u+2, n.u+2))
+		
+	if (n.u > 0) {
+		XX1[1, 2:(n.u+2)] = c(SY1[U.gam], mu.g)
+		XX1[2:(n.u+2), 1] = c(SY1[U.gam], mu.g)
+		XX1[n.u+2, 2:(n.u+2)] = c(SYX[U.gam], sxx)
+		XX1[2:(n.u+1), n.u+2] = SYX[U.gam]
+		XX1[2:(n.u+1), 2:(n.u+1)] = SYY[U.gam, U.gam]
+	
+		XX0 <- XX1[1:(n.u+1), 1:(n.u+1)]	
+	} else {
+		XX1[1,2] <- mu.g
+		XX1[2,1] <- mu.g
+		XX1[2,2] <- sxx
+		
+		XX0 <- matrix(1, 1, 1)
+	}
+					
+	##Build RSS1 = 1/n.ind * RSS(Y_D | [1 Y_u X_s], K) and RSS0 = 1/n.ind * RSS(Y_D | [1 Y_u], 0)##
+	
+	vec <- c(SY1[D.gam], SYY[D.gam, U.gam], SYX[D.gam])
+	
+	if (n.u > 0) {
+		RSS0 <- SYY[D.gam, D.gam] - sum( vec[-(n.u+2)] * solve(XX0, vec[-(n.u+2)]) )
+	} else {
+		RSS0 <- SYY[D.gam, D.gam] - vec[1]*vec[1]
+	}		
+	
+	log.bf <- rep(0, length=length(var.a))
+	for (s in 1:length(var.a)) {
+		tmp.mat <- XX1
+		tmp.mat[n.u+2, n.u+2] <- tmp.mat[n.u+2, n.u+2] + 1/var.a[s]/n.ind		#tmp.mat = XX1 + 1/n.ind * K
+		RSS1 <- SYY[D.gam, D.gam] - sum(vec * solve(tmp.mat, vec))
+		log.bf[s] <- length(D.gam)/2 * ( -log(var.a[s]) + log(det(XX0)) - log(n.ind) - log(det(tmp.mat)) ) + (n.ind + m - (n.nei + 1) + n.u + length(D.gam))/2 * ( log(RSS0) - log(RSS1) )
+	}
+	
+	##Compute log Bayes Factor##
+	
+	logBF = log( sum(weights*exp(log.bf - max(log.bf))) ) + max(log.bf)
+
+	log10BF <- logBF/log(10)		#Log10 BF
+	
+	return(list(log10BF = log10BF, D.ind = D.gam, I.ind = (1:(n.nei+1))[ -c(D.gam,U.gam) ], U.ind=U.gam))
 }
 
---
+++ R/directionality_CGM.R
@@ -0,0 +1,50 @@
+###The purpose of this script is to run a Gibbs sampler to determine which neighbors of a gene g with cis-eQTL s are unaffected and which are indirectly affected
+###Created 151211
+
+###Input: sufficient statistic, direct effect index, number of independent observations, sigma.a, simgma.a weights, Wishart Prior m, theta
+#If beta = F, then the theta[1] is the prior probability a node is unaffected
+
+###Output: Posterior probability that each index is an INDIRECT effect; The NA is the direct effect
+
+
+Gibbs.dir <- function(n.iter, n.burn, suff.stat, D.gam, n.ind, sigma.a, weights.sigma, m, theta, beta=T) {			#P(Node is Unaffected) ~ Beta(theta[1], theta[2])
+	n.nei <- dim(suff.stat$SYY)[1] - 1
+	
+	ind.infer <- rep(0, (n.nei+1))			##0 is unaffected, 1 is indirectly affected, 2 is directly affected
+	ind.infer[D.gam] <- 2
+	
+	iterate.index <- which(ind.infer != 2)
+	post.mean <- rep(0, n.nei+1); post.mean[D.gam] <- NA				#Posterior mean that index is indirectly affected
+	
+	for (i in 1:n.iter) {
+		
+		for (j in iterate.index) {
+			tmp.0 <- ind.infer
+			tmp.0[j] <- 0					##Make jth index 0, i.e. unaffected
+			
+			tmp.1 <- ind.infer
+			tmp.1[j] <- 1					##Make jth index 1, i.e. indirectly affected
+			
+			log10bfU <- Log10BF.sing(suff.stat, D.gam, which(tmp.0 == 0), n.ind, sigma.a, weights.sigma, m)$log10BF
+			log10bfI <- Log10BF.sing(suff.stat, D.gam, which(tmp.1 == 0), n.ind, sigma.a, weights.sigma, m)$log10BF
+			
+			if (beta) {
+				pU.j <- ( theta[1] + length(which(tmp.1 == 0)) )/(sum(theta) + n.nei - 1)			#If we use a beta prior, find p(Node j is Unaffected | Rest of Node Assignments)
+			} else {
+				pU.j <- theta[1]
+			}
+			
+			C.j <- max(log10bfU, log10bfI)
+			post.probU.j <- pU.j*exp( log10bfU - C.j )/( pU.j*exp( log10bfU - C.j ) + (1-pU.j)*exp( log10bfI - C.j ) )
+			
+			is.I.j <- rbinom(1, 1, 1 - post.probU.j)			#Gibbs sampler
+			ind.infer[j] <- is.I.j
+			if (i > n.burn) {
+				post.mean[j] <- post.mean[j] + is.I.j/(n.iter - n.burn)
+			}		
+		}
+		
+	}
+	
+	return(list(post.mean=post.mean, D.ind=D.gam))
+}
